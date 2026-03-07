daf473d7e3503c37eaf038e954b2f4ad63f32af3
tatiana-sova <tatisuomalainen@gmail.com>
2026-03-08 01:35:31 +0300

    Update Form1.Designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,16 +1,1 @@
 
-        private Button button1;
-        private Button button2;
-        private Button button3;
-        private Label label1;
-        private Label labelHumValue;
-        private Label labelTempValue;
-        private Label label4;
-        private Button buttonOpenFile;
-        private Label labelLastUpdate;
-        private Button minute;
-        private Button hour;
-        private Button live;
-    }
-}
-
